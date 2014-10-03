cceb7e94493c00bf83e0b6dccb63d17915f18a0c
De Lène Mirouze <fmirouze@expertime.com>
2014-10-03 15:55:53 +0200

    init

---
+++ ApmPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApmPattern
+{
+    class Program  
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
